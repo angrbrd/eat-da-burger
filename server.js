3339f09aa54874b160948666e0be36c79ef4e76d
Anna D Derbakova <hiopa16@gmail.com>
2017-03-18 01:20:04 -0400

    Adding working server file, tested with Postman

---
+++ server.js
@@ -0,0 +1,29 @@
+// Pull in required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the 'public' directory
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+// Set Handlebars as the view engine
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+app.listen(port);
